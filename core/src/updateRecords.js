5024cf91d8348b7105ae4a4c7b9d812a4403172c
抹露茶柒 <me@MoruTeaven.com>
2026-08-21 18:17:46 +0800

    fix: 调整 2.4.1 更新日志文案

--- core/src/updateRecords.js
+++ core/src/updateRecords.js
@@ -5,9 +5,9 @@ export const updateRecords = [
     changes: {
       added: [],
       fixed: [
-        { text: '修复通过功能指令（右键图片编辑、拖入图片文件等）进入插件时图片无法加载的问题', platforms: ['utools', 'ztools'] },
+        { text: '修复通过功能指令进入插件时图片无法加载的问题', platforms: ['utools', 'ztools'] },
         { text: '修复部分字体名称显示不正确的问题', platforms: null },
-        { text: '修复 Windows 平台无法获取系统字体列表的问题', platforms: ['utools', 'ztools'] },
+        { text: '修复部分 Windows 平台无法获取系统字体列表的问题', platforms: ['utools', 'ztools'] },
         { text: '修复大图片加载时可能超时失败的问题', platforms: ['utools', 'ztools'] },
         { text: '修复重复进入插件时图片可能无法加载的问题', platforms: ['utools', 'ztools'] }
       ],
